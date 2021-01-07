42efe9b37a750251c98129b6de6bed42d53b1f28
Kaio Pedromonico <kpedromonico@gmail.com>
2021-01-07 10:21:47 +0100

    Including project on Git with the Mediator pattern

---
+++ DesignPatterns/Behavioural/Mediator/Mediator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Behavioural.Mediator
+{
+    public class Mediator
+    {
+        private Presenter _presenter;
+        private List<Attendee> _attendees;
+
+
+        public void AddAttendees(Attendee attendee)
+        {
+
+            if (_attendees == null)
+            {
+                _attendees = new List<Attendee> { attendee };
+            }
+            else
+            {
+                _attendees.Add(attendee);
+            }
+        }
+
+        public int GetNumberOfAttendees() => _attendees.Count;
+
+        public void SetPresenter (Presenter presenter) => _presenter = presenter;        
+
+        internal void NotifyImageUpdate(string fakeImageName)
+        {
+            foreach(var attendee in _attendees)
+            {
+                attendee.NotifyImageUpdate(fakeImageName);
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Behavioural/Mediator/PresentationMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Behavioural.Mediator
+{
+    public abstract class PresentationMember
+    {
+        protected Mediator _mediator;
+
+        public PresentationMember(Mediator mediator)
+        {
+            this._mediator = mediator;
+        }        
+    }
+}
