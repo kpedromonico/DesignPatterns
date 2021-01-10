6546ee6617219535d59ef4ee525951defd731ba6
Kaio Pedromonico <kpedromonico@gmail.com>
2021-01-10 10:39:21 +0100

    Added Abstract Factory, code based on GoF example

---
+++ DesignPatterns/Creational/AbstractFactory/ContinentFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Creational.AbstractFactory
+{
+    public abstract class ContinentFactory
+    {
+        public abstract Herbivore CreateHerbivore();
+
+        public abstract Carnivore CreateCarnivore();
+    }
+}
